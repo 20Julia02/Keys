c30e0e17edaabd57a1f7ae3a796d8e8fcb41fcf6
Julia Białas <20juliabialas02@gmail.com>
2024-05-16 23:59:05 +0200

    initial commit

---
+++ app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from .routers import user
+
+app = FastAPI()
+
+app.include_router(user.router)
+
+
+@app.get("/")
+def root():
+    return {"message": "Server is running"}
