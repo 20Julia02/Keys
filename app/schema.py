56b19794402204e72524c309d49686e9f25ff86c
Julia Białas <20juliabialas02@gmail.com>
2024-05-17 23:01:47 +0200

    Authorization and sending token

--- app/schema.py
+++ app/schema.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from pydantic import BaseModel, EmailStr
 
 
@@ -35,3 +36,12 @@ class UserOut(BaseModel):
 
     class Config:
         from_attributes = True
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    id: Optional[int] = None
