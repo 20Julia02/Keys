0c47652b8d82b1667a018378ebf4764075f764ec
Julia Białas <20juliabialas02@gmail.com>
2024-10-23 14:43:44 +0200

    on delete added

--- app/models.py
+++ app/models.py
@@ -22,6 +22,7 @@ class Base(DeclarativeBase):
 
 class TokenBlacklist(Base):
     __tablename__ = 'token_blacklist'
+
     id: Mapped[intpk]
     token: Mapped[str] = mapped_column(String(255), unique=True)
     added_at: Mapped[Optional[timestamp]]
@@ -41,16 +42,19 @@ class DeviceType(enum.Enum):
 
 class Device(Base):
     __tablename__ = "device"
+
     id: Mapped[intpk]
     code: Mapped[str] = mapped_column(String(50), unique=True)
     dev_type: Mapped[DeviceType]
-    room_id: Mapped[int] = mapped_column(ForeignKey("room.id"))
+    room_id: Mapped[Optional[int]] = mapped_column(ForeignKey("room.id"))
     dev_version: Mapped[DeviceVersion]
 
     room: Mapped["Room"] = relationship(back_populates="devices")
     notes: Mapped[List["DeviceNote"]] = relationship(back_populates="device")
-    device_operations: Mapped[List["DeviceOperation"]] = relationship(back_populates="device")
-    unapproved_operations: Mapped[List["UnapprovedOperation"]] = relationship(back_populates="device")
+    device_operations: Mapped[List["DeviceOperation"]
+                              ] = relationship(back_populates="device")
+    unapproved_operations: Mapped[List["UnapprovedOperation"]] = relationship(
+        back_populates="device")
 
     __table_args__ = (UniqueConstraint(
         "dev_type", "room_id", "dev_version", name="uix_device"),)
@@ -61,33 +65,42 @@ OperationType = Literal["pobranie", "zwrot"]
 
 class UnapprovedOperation(Base):
     __tablename__ = "operation_unapproved"
+
     id: Mapped[intpk]
-    device_id: Mapped[int] = mapped_column(ForeignKey("device.id"))
-    session_id: Mapped[int] = mapped_column(ForeignKey("session.id"))
+    device_id: Mapped[int] = mapped_column(
+        ForeignKey("device.id", ondelete='CASCADE'))
+    session_id: Mapped[int] = mapped_column(
+        ForeignKey("session.id", ondelete='CASCADE'))
     operation_type: Mapped[OperationType]
     entitled: Mapped[bool]
     timestamp: Mapped[Optional[timestamp]]
 
-    session: Mapped["IssueReturnSession"] = relationship(back_populates="unapproved_operations")
-    device: Mapped["Device"] = relationship(back_populates="unapproved_operations")
+    session: Mapped["IssueReturnSession"] = relationship(
+        back_populates="unapproved_operations")
+    device: Mapped["Device"] = relationship(
+        back_populates="unapproved_operations")
 
 
 class DeviceOperation(Base):
     __tablename__ = "device_operation"
 
     id: Mapped[intpk]
-    device_id: Mapped[int] = mapped_column(ForeignKey("device.id"))
-    session_id: Mapped[int] = mapped_column(ForeignKey("session.id"))
+    device_id: Mapped[int] = mapped_column(
+        ForeignKey("device.id", ondelete='CASCADE'))
+    session_id: Mapped[int] = mapped_column(
+        ForeignKey("session.id", ondelete='CASCADE'))
     operation_type: Mapped[OperationType]
     entitled: Mapped[bool]
     timestamp: Mapped[Optional[timestamp]]
 
     device: Mapped["Device"] = relationship(back_populates="device_operations")
-    session: Mapped["IssueReturnSession"] = relationship(back_populates="device_operations")
+    session: Mapped["IssueReturnSession"] = relationship(
+        back_populates="device_operations")
 
 
 class BaseUser(Base):
     __tablename__ = "base_user"
+
     id: Mapped[intpk]
     user_type = Column(String(50))
 
@@ -97,7 +110,8 @@ class BaseUser(Base):
     }
 
     notes: Mapped[List["UserNote"]] = relationship(back_populates="user")
-    sessions: Mapped[List["IssueReturnSession"]] = relationship(back_populates="user")
+    sessions: Mapped[List["IssueReturnSession"]
+                     ] = relationship(back_populates="user")
 
 
 class UserRole(enum.Enum):
@@ -114,7 +128,9 @@ class Faculty(enum.Enum):
 
 class User(BaseUser):
     __tablename__ = 'user'
-    id: Mapped[intpk] = mapped_column(ForeignKey('base_user.id'))
+
+    id: Mapped[intpk] = mapped_column(
+        ForeignKey('base_user.id', ondelete='CASCADE'))
     name: Mapped[str] = mapped_column(String(50))
     surname: Mapped[str] = mapped_column(String(50))
     role: Mapped[UserRole]
@@ -128,13 +144,17 @@ class User(BaseUser):
         'polymorphic_identity': 'user'
     }
 
-    permissions: Mapped[List["Permission"]] = relationship(back_populates="user")
-    sessions: Mapped[List["IssueReturnSession"]] = relationship(back_populates="concierge")
+    permissions: Mapped[List["Permission"]
+                        ] = relationship(back_populates="user")
+    sessions: Mapped[List["IssueReturnSession"]
+                     ] = relationship(back_populates="concierge")
 
 
 class UnauthorizedUser(BaseUser):
     __tablename__ = "unauthorized_user"
-    id: Mapped[intpk] = mapped_column(ForeignKey('base_user.id'))
+
+    id: Mapped[intpk] = mapped_column(
+        ForeignKey('base_user.id', ondelete='CASCADE'))
     name: Mapped[str] = mapped_column(String(50))
     surname: Mapped[str] = mapped_column(String(50))
     email: Mapped[str] = mapped_column(String(50), unique=True)
@@ -152,16 +172,20 @@ class IssueReturnSession(Base):
     __tablename__ = "session"
 
     id: Mapped[intpk]
-    user_id: Mapped[int] = mapped_column(ForeignKey("base_user.id"))
-    concierge_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    user_id: Mapped[Optional[int]] = mapped_column(ForeignKey("base_user.id"))
+    concierge_id: Mapped[Optional[int]] = mapped_column(ForeignKey("user.id"))
     start_time: Mapped[datetime.datetime]
     end_time: Mapped[Optional[datetime.datetime]]
     status: Mapped[SessionStatus]
 
-    device_operations: Mapped[List["DeviceOperation"]] = relationship(back_populates="session")
-    unapproved_operations: Mapped[List["UnapprovedOperation"]] = relationship(back_populates="session")
-    user: Mapped["BaseUser"] = relationship(foreign_keys=[user_id], back_populates="sessions")
-    concierge: Mapped["User"] = relationship(foreign_keys=[concierge_id], back_populates="sessions")
+    device_operations: Mapped[List["DeviceOperation"]
+                              ] = relationship(back_populates="session")
+    unapproved_operations: Mapped[List["UnapprovedOperation"]] = relationship(
+        back_populates="session")
+    user: Mapped["BaseUser"] = relationship(
+        foreign_keys=[user_id], back_populates="sessions")
+    concierge: Mapped["User"] = relationship(
+        foreign_keys=[concierge_id], back_populates="sessions")
 
 
 class Room(Base):
@@ -170,15 +194,19 @@ class Room(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     number: Mapped[str] = mapped_column(String(20), unique=True)
 
-    permissions: Mapped[List["Permission"]] = relationship(back_populates="room")
+    permissions: Mapped[List["Permission"]
+                        ] = relationship(back_populates="room")
     devices: Mapped[List["Device"]] = relationship(back_populates="room")
 
 
 class Permission(Base):
     __tablename__ = "permission"
+
     id: Mapped[intpk]
-    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
-    room_id: Mapped[int] = mapped_column(ForeignKey("room.id"))
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("user.id", ondelete='RESTRICT'))
+    room_id: Mapped[int] = mapped_column(
+        ForeignKey("room.id", ondelete='RESTRICT'))
     start_reservation: Mapped[datetime.datetime]
     end_reservation: Mapped[datetime.datetime]
 
@@ -188,8 +216,10 @@ class Permission(Base):
 
 class DeviceNote(Base):
     __tablename__ = "device_note"
+
     id: Mapped[intpk]
-    device_id: Mapped[int] = mapped_column(ForeignKey("device.id"))
+    device_id: Mapped[int] = mapped_column(
+        ForeignKey("device.id", ondelete='CASCADE'))
     note: Mapped[str]
     timestamp: Mapped[Optional[timestamp]]
 
@@ -200,7 +230,8 @@ class UserNote(Base):
     __tablename__ = "user_note"
 
     id: Mapped[intpk]
-    user_id: Mapped[int] = mapped_column(ForeignKey("base_user.id"))
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("base_user.id", ondelete='CASCADE'))
     note: Mapped[str]
     timestamp: Mapped[Optional[timestamp]]
 
