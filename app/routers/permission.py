2ff2ed0279bddf67e5976bd1b4e5951994e2f968
20Julia02 <20juliabialas02@gmail.com>
2024-10-27 19:12:58 +0100

    some bugs fixed

--- app/routers/permission.py
+++ app/routers/permission.py
@@ -18,7 +18,7 @@ router = APIRouter(
 def get_permissions(
     user_id: Optional[int] = None,
     room_id: Optional[int] = None,
-    date: Optional[datetime.date] = datetime.date.today(),
+    date: Optional[datetime.date] = None,
     start_time: Optional[datetime.time] = None,
     current_concierge=Depends(oauth2.get_current_concierge),
     db: Session = Depends(database.get_db)
